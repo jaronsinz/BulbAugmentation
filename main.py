ff4e2931a64de24b1c79cf40f1f4cb92c38a1f2e
Jaron Sinz <wi21244@lehre.dhbw-stuttgart.de>
2023-10-24 14:26:48 +0200

    added files

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import cv2
 img_original = cv2.imread("input/images/background_test.jpg")
 img_yellow_bulb = cv2.imread("bulb_images/yellow_bulb.png")
 
+
 cv2.imshow("Image", img_original)
 cv2.waitKey(0)
