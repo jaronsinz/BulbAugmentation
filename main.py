9bbd5403fb28cf301e9246164aeb5f9ea6822a62
Jaron Sinz <wi21244@lehre.dhbw-stuttgart.de>
2023-10-26 13:21:51 +0200

    reading coordinates from json

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import numpy as np
 import cv2
 import cvzone
 import random
+import json
 
+#reading json
+with open("input/labels/test_json.json", "r") as file_path:
+    label_file = json.load(file_path)
 
 #reading all images
 img_original = cv2.imread("input/images/background_test.jpg")
@@ -14,6 +18,9 @@ img_green_bulb = cv2.imread("bulb_images/green_bulb.png", cv2.IMREAD_UNCHANGED)
 #choosing random bulb to overlay
 bulb_list = [img_red_bulb, img_yellow_bulb, img_green_bulb]
 random_number = random.randint(0, 2)
+
+#getting position to overlay from label_file
+coordinates = label_file["openlabel"]["frames"]["0"]["objects"]["12345678-1234-1234-1234-123456789123"]["object_data"]["bbox"][0]["val"]
 
 #overlaying image
 img_result = cvzone.overlayPNG(img_original, bulb_list[random_number], [0, 0])
